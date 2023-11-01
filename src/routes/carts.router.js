da33d8067b926bcbb0ec7d07ec0d7113c01f8394
jmcrisol <juanmanuelcrisol@gmail.com>
2023-10-31 23:48:27 -0300

    AJustes

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,7 +1,113 @@
-import express from 'express';
-import { promises as fs } from 'fs';
-const cartsRouter = express.Router();
+import { Router } from 'express';
+import fs from 'fs';
+
+const cartsRouter = Router();
+
+// Ruta raíz POST / para crear un nuevo carrito
+cartsRouter.post('/', (req, res) => {
+  const newCart = {
+    id: generateCartId(), // Implementa una función para generar un ID único
+    products: [],
+  };
+
+  // Guarda el nuevo carrito en el sistema de archivos ( carrito.json)
+  saveCart(newCart);
+
+  res.status(201).json(newCart);
+});
+
+// Ruta GET /:cid para listar los productos en un carrito específico
+cartsRouter.get('/:cid', (req, res) => {
+  const cartId = req.params.cid;
+  const cart = loadCart(cartId); // Implementa una función para cargar el carrito desde el sistema de archivos
+
+  if (cart) {
+    res.json(cart.products);
+  } else {
+    res.status(404).json({ message: 'Carrito no encontrado' });
+  }
+});
+
+// Ruta POST /:cid/product/:pid para agregar un producto a un carrito
+cartsRouter.post('/:cid/product/:pid', (req, res) => {
+  const cartId = req.params.cid;
+  const productId = req.params.pid;
+  const quantity = 1; // Por ahora, se agrega un producto de uno en uno
+
+  const cart = loadCart(cartId); // Cargar el carrito desde el sistema de archivos
+
+  if (cart) {
+    // Verifica si el producto ya existe en el carrito
+    const existingProduct = cart.products.find((product) => product.product === productId);
+
+    if (existingProduct) {
+      // Si el producto ya existe, incrementa la cantidad
+      existingProduct.quantity += quantity;
+    } else {
+      // Si el producto no existe, agrégalo al carrito
+      cart.products.push({ product: productId, quantity });
+    }
+
+    // Guarda el carrito actualizado en el sistema de archivos
+    saveCart(cart);
+
+    res.status(201).json(cart);
+  } else {
+    res.status(404).json({ message: 'Carrito no encontrado' });
+  }
+});
+
+// Variable para mantener el último ID de carrito asignado
+let lastCartId = getLastCartId();
+
+// Función para obtener el último ID de carrito guardado en './carrito.json'
+function getLastCartId() {
+  try {
+    const cartData = fs.readFileSync('./carrito.json', 'utf8');
+    const cart = JSON.parse(cartData);
+    return parseInt(cart.id) || 0;
+  } catch (err) {
+    console.error('Error al obtener el último ID de carrito desde carrito.json', err);
+    return 0; // Si ocurre un error, se establece 0 como valor predeterminado
+  }
+}
+
+// Función para generar un ID único para el carrito
+function generateCartId() {
+  lastCartId++; // Incrementa el último ID
+  return lastCartId.toString(); // Convierte el ID a cadena (puede ser número o cadena, según tu preferencia)
+}
 
 
+// Función para cargar un carrito desde el sistema de archivos
+function loadCart(cartId) {
+    try {
+      const cartData = fs.readFileSync('./carrito.json', 'utf8'); // Lee el contenido del archivo
+      const cart = JSON.parse(cartData); // Parsea el contenido del archivo JSON
+  
+      // Verifica si el carrito coincide con el cartId proporcionado
+      if (cart.id === cartId) {
+        return cart;
+      } else {
+        return null; // El carrito no coincide con el cartId
+      }
+    } catch (err) {
+      console.error('Error al cargar el carrito desde el sistema de archivos', err);
+      return null; // En caso de error, devuelve null
+    }
+  }
 
-export { cartsRouter };
+// Función para guardar un carrito en el sistema de archivos
+function saveCart(cart) {
+    const cartData = JSON.stringify(cart, null, 2); // Convierte el carrito a formato JSON con formato legible
+   
+    fs.appendFile('./carrito.json', cartData, (err) => {
+      if (err) {
+        console.error('Error al guardar el carrito en el sistema de archivos', err);
+      } else {
+        console.log('Carrito guardado exitosamente en el archivo carrito.json');
+      }
+    });
+  }
+
+export default cartsRouter;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,104 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+
 const productsRouter = express.Router();
 
+// Leer el archivo JSON
+let products = [];
+
+async function readProductsFile() {
+  try {
+    const data = await fs.readFile('./products.json', 'utf8');
+    products = JSON.parse(data);
+  } catch (err) {
+    console.error('Error al leer el archivo JSON:', err);
+  }
+}
+
+readProductsFile(); // Lee el archivo al inicio
+
+// Ruta para listar todos los productos con límite
+productsRouter.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit, 10); // Parsea el parámetro 'limit' a un número entero
+    if (isNaN(limit) || limit <= 0) {
+      // Si 'limit' no es un número válido o es menor o igual a 0, muestra todos los productos
+      res.json(products);
+    } else {
+      // Muestra la cantidad limitada de productos
+      res.json(products.slice(0, limit));
+    }
+  });
 
 
+// Ruta para traer un producto por ID
+productsRouter.get('/:pid', (req, res) => {
+  const productId = req.params.pid;
+  const product = products.find(p => p.id == productId);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send('Producto no encontrado');
+  }
+});
 
+// Ruta para agregar un nuevo producto
+productsRouter.post('/', (req, res) => {
+  const newProduct = req.body;
+  // Validar que todos los campos obligatorios estén presentes
+  if (!newProduct.title || !newProduct.description || !newProduct.code || !newProduct.price || !newProduct.stock || !newProduct.category) {
+    res.status(400).send('Faltan campos obligatorios en la solicitud');
+  } else {
+    // Generar un nuevo ID (esto depende de cómo estás generando los IDs)
+    newProduct.id = generateNewProductId();
+    newProduct.status = true; // Status es true por defecto
+    products.push(newProduct);
+    saveProductsToFile();
+    res.status(201).json(newProduct);
+  }
+});
 
-export { productsRouter };
+// Ruta para actualizar un producto por ID
+productsRouter.put('/:pid', (req, res) => {
+  const productId = req.params.pid;
+  const updatedProduct = req.body;
+  const existingProductIndex = products.findIndex(p => p.id == productId);
+  if (existingProductIndex !== -1) {
+    // No actualizar el ID
+    updatedProduct.id = productId;
+    products[existingProductIndex] = updatedProduct;
+    saveProductsToFile();
+    res.json(updatedProduct);
+  } else {
+    res.status(404).send('Producto no encontrado');
+  }
+});
+
+// Ruta para eliminar un producto por ID
+productsRouter.delete('/:pid', (req, res) => {
+  const productId = req.params.pid;
+  const productIndex = products.findIndex(p => p.id == productId);
+  if (productIndex !== -1) {
+    products.splice(productIndex, 1);
+    saveProductsToFile();
+    res.send('Producto eliminado');
+  } else {
+    res.status(404).send('Producto no encontrado');
+  }
+});
+
+// Función para guardar los productos en el archivo
+async function saveProductsToFile() {
+  try {
+    await fs.writeFile('./products.json', JSON.stringify(products, null, 2), 'utf8');
+  } catch (err) {
+    console.error('Error al guardar en el archivo JSON:', err);
+  }
+}
+
+// Genera un nuevo ID (esto es un ejemplo, puedes ajustarlo según tu necesidad)
+function generateNewProductId() {
+  const maxId = Math.max(...products.map(p => p.id), 0);
+  return maxId + 1;
+}
+
+export default productsRouter;
