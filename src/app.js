8c49b9a08b8bf3ac876656bbeaa1a6094f72adc2
jmcrisol <juanmanuelcrisol@gmail.com>
2023-11-30 21:44:07 -0300

    Revert "ChatMongoDB"
    This reverts commit 7225a5813560df6a94ad2881528e9b67bb0a8867.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
-import chatRouter from './routes/chat.router.js';
 import __dirname from "./utils.js";
 import handlebars from "express-handlebars";
 import {Server} from "socket.io";
 import viewRouter from "./routes/views.router.js";
 import homeRouter from './routes/home.router.js';
 import realtimeproducts from './routes/realtimeproducts.router.js';
-import messageModel from './dao/models/messages.model.js';
+import path from 'path';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -29,36 +29,8 @@ app.use('/', viewRouter);
 io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
 
-    socket.on('message', async (data) => {
-        try {
-            // Guardar el mensaje en MongoDB
-            const newMessage = new messageModel(data);
-            await newMessage.save();
-    
-            // Obtener el historial de mensajes actualizado
-            const messages = await messageModel.find({});
-            
-            // Emitir el historial de mensajes actualizado a todos los clientes
-            io.emit('messageLogs', messages || []); // Enviar un array vacío si no hay mensajes
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-    socket.on("auth", (username) => {
-        // Emitir el historial de mensajes al cliente que se autentica
-        messageModel.find({})
-            .then((messages) => {
-                socket.emit("messageLogs", messages);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    
-        // Emitir mensaje de usuario conectado a todos los clientes excepto al que se autentica
-        socket.broadcast.emit("userConnected", username);
-    });
 });
+
 
 
 // Conectar los routers a las rutas principales
@@ -66,8 +38,6 @@ app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/home', homeRouter);
 app.use('/realtimeproducts', realtimeproducts);
-app.use('/chat', chatRouter);
-
 
 //Mensaje para cuelquier ruta erronea
 app.use((req, res) => {
--- src/routes/chat.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const chatRouter = express.Router();
-
-chatRouter.get('/', (req, res)=>{
-    res.render('layouts/chat',{});
-});
-
-export default chatRouter;
